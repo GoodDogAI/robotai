39ea02855099a88edb8ba01bf1d59d6afc45e263
Jake <jakepoz@gmail.com>
2022-07-05 14:43:13 -0700

    Refactoring things with pydantic

--- src/logutil.py
+++ src/logutil.py
@@ -1,8 +1,16 @@
 import os
 import hashlib
 import json
+import logging
 
+from typing import List, Dict
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, parse_file_as
+
+logger = logging.getLogger(__name__)
 DATA_FILE = "_hashes.json"
+
 
 def sha256(filename: str) -> str:
     sha256_hash = hashlib.sha256()
@@ -13,11 +21,17 @@ def sha256(filename: str) -> str:
     return sha256_hash.hexdigest()
 
 
+class LogSummary(BaseModel):
+    filename: str
+    sha256: str
+    last_modified: int
+
+
 # Allows for quick and cached access to the SHA256 hash of a bunch of log files
 class LogHashes:
     dir: str
     extension: str=".log"
-    hashes: dict={}
+    hashes: Dict[str, LogSummary] = {}
 
     def __init__(self, dir):
         self.dir = dir
@@ -26,8 +40,7 @@ class LogHashes:
     def update(self):
         path = os.path.join(self.dir, DATA_FILE)
         if os.path.exists(path):
-            with open(path, "r") as f:
-                self.hashes = json.load(f)
+            self.hashes = parse_file_as(Dict[str, LogSummary], path)
         else:
             self.hashes = {}
 
@@ -37,20 +50,21 @@ class LogHashes:
             filepath = os.path.join(self.dir, file)
             mtime = round(os.path.getmtime(filepath) * 1e9)
 
-            if file in self.hashes and self.hashes[file]["last_modified"] == mtime:
+            if file in self.hashes and self.hashes[file].last_modified == mtime:
                 continue
 
-            self.hashes[file] = {
-                "last_modified": mtime,
-                "sha256": sha256(filepath),
-            }
+            logger.info(f"Hashing {filepath}")
+            self.hashes[file] = LogSummary(filename=file, sha256=sha256(filepath), last_modified=mtime)
 
         with open(path + "_temp", "w") as f:
-            json.dump(self.hashes, f)
+            json.dump(jsonable_encoder(self.hashes), f)
 
         if os.path.exists(path):
             os.remove(path)
         os.rename(path + "_temp", path)
+
+    def values(self) -> List[LogSummary]:
+        return self.hashes.values()
 
     def hash_exists(self, hash:str) -> bool:
         return hash in self.hashes
--- src/web/main.py
+++ src/web/main.py
@@ -1,33 +1,30 @@
 import os
+from typing import List
+
 from .config import RECORD_DIR
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from src.logutil import LogHashes
 
 app = FastAPI()
+loghashes = LogHashes(RECORD_DIR)
 
 
 
 @app.get("/logs")
-async def list_logs():
-    result = []
-
-    for file in os.listdir(RECORD_DIR):
-        if not file.endswith(".log"):
-            continue
-
-        result.append({
-            "filename": os.path.basename(file),
-            "hash": "0",
-        })
-
-    return result
+async def list_logs() -> List[LogSummary]:
+    return loghashes.values()
 
 
 @app.get("/logs/exists/{sha256}")
 async def log_exists(sha256: str):
-    return False
+    return loghashes.hash_exists(sha256)
 
 @app.put("/logs/{logfile}")
-async def put_log(logfile: str) -> bool:
+async def put_log(logfile: UploadFile) -> bool:
+    # Make sure the hash doesn't exist already
+
+    # Copy it over to the log location, adding new name if necessary
     return False
 
 @app.get("/logs/{logfile}")
