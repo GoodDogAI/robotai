d4ce0069c43f6df639c231079b166357269c70d7
Jake Poznanski <jakepoz@gmail.com>
2022-07-27 14:14:38 -0700

    Working on manager to launch python processes

--- src/realtime/loguploader.py
+++ src/realtime/loguploader.py
@@ -1,20 +1,34 @@
 import re
 import os
+import time
 import requests
+
+from typing import Dict, Any
 
 # Watches for newly completed log files in the logging directory, and uploads them
 
 from src.logutil import LogHashes
 
-config_vars = {}
-with open(os.path.join(os.path.dirname(__file__), "..", "include", "config.h")) as f:
-    b = re.findall(r"\#define +([a-zA-Z0-9_]+) +\"?([0-9a-zA-Z_/\-\'\:]+)\"?", f.read())
+def load_realtime_config() -> Dict[str, Any]:
+    config_vars = {}
+    with open(os.path.join(os.path.dirname(__file__), "..", "include", "config.h")) as f:
+        b = re.findall(r"\#define +([a-zA-Z0-9_]+) +\"?([0-9a-zA-Z_/\-\'\:]+)\"?", f.read())
 
-    for match in b:
-        config_vars[match[0]] = match[1]
+        for match in b:
+            config_vars[match[0]] = match[1]
 
-print(config_vars)
+    return config_vars
 
-lh = LogHashes(config_vars["LOG_PATH"])
+
+CONFIG = load_realtime_config()
+
+lh = LogHashes(CONFIG["LOG_PATH"])
 
 print(lh)
+
+def main():
+    while True:
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
--- src/realtime/manager.py
+++ src/realtime/manager.py
@@ -1,45 +1,110 @@
 import os
 import asyncio
+import importlib
+import logging
+import multiprocessing
+from setproctitle import setproctitle 
 
+# Manager script to schedule and run a bunch of workers with various configurations
+# Loosely based on: https://github.com/commaai/openpilot/blob/master/selfdrive/manager
+logger = logging.getLogger(__name__)
+
+def pythonlauncher(name: str, module: str):
+    # import the process
+    logger.info(f"Importing {name} {module}")
+    mod = importlib.import_module(module)
+
+    # rename the process
+    setproctitle(name)
+
+    # exec the process
+    logger.info(f"Launching {name} {module}")
+    getattr(mod, 'main')()
 
 class ManagerProcess:
     name: str = ""
+    running: bool = False
     p = None
-
+    
     def __init__(self, name):
         self.name = name
 
+    async def start(self):
+        raise NotImplementedError()
+
+    async def join(self):
+        raise NotImplementedError()
+
+    def kill(self):
+        raise NotImplementedError()
 
 class PythonProcess(ManagerProcess):
-    pass
+    module: str = ""
+
+    def __init__(self, name, module):
+        super().__init__(name)
+        self.module = module
+
+    async def start(self):
+        self.p = multiprocessing.Process(name=self.name, target=pythonlauncher, args=(self.name, self.module))
+        self.running = True
+        self.p.start()
+
+    async def join(self):
+        while True:
+            self.p.join(timeout=1)
+            yield
+
+        self.running = False
+
+    def kill(self):
+        self.p.kill()
+        self.running = False
 
 class NativeProcess(ManagerProcess):
     def __init__(self, name):
         super().__init__(name)
 
+    async def start(self):
+        self.p = await asyncio.create_subprocess_exec(os.path.abspath(f"build/{self.name}"))
+        self.running = True
+
+    async def join(self):
+        await self.p.wait()
+        self.running = False
+
+    def kill(self):
+        self.p.kill()
+        self.running = False
+
 procs = [
     NativeProcess("camerad"), 
-    NativeProcess("encoderd"),
-    NativeProcess("loggerd"),
+    # NativeProcess("encoderd"),
+    # NativeProcess("loggerd"),
+    PythonProcess("loguploader", "src.realtime.loguploader"),
 ]
 
 async def main():
-    print("Starting manager...")
+    logger.warning("Starting manager...")
 
     for proc in procs:
-        print(f"Starting {proc.name}...")
-        proc.p = await asyncio.create_subprocess_exec(os.path.abspath(f"build/{proc.name}"))
+        logger.warning(f"Starting {proc.name}...")
+        await proc.start()
 
-    await asyncio.wait([proc.p.wait() for proc in procs], return_when=asyncio.FIRST_COMPLETED)
+    done, pending = await asyncio.wait([proc.join() for proc in procs], return_when=asyncio.FIRST_COMPLETED)
+
+    logger.warning(f"Done: {done}")
 
     for proc in procs:
         try:
-            await proc.p.kill()
-            print(f"Killed {proc.name}")
-        except:
-            print(f"Could not kill {proc.name}")
+            if proc.running:
+                print("killing ", proc.name)
+                proc.kill()
+                logger.info(f"Killed {proc.name}")
+        except Exception as e:
+            logger.exception(f"Could not kill {proc.name}")
 
-    print("Manager finished")
+    logger.info("Manager finished")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
