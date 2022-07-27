fdfe28a699d016d1d1d0913b49c47ac6a7c094e0
Jake Poznanski <jakepoz@gmail.com>
2022-07-27 15:19:11 -0700

    Working on the loguploading part now.

--- src/include/config.h
+++ src/include/config.h
@@ -6,7 +6,7 @@
 
 #define LOG_DURATION_SECONDS 60
 #define LOG_PATH "/media/card"
-#define LOG_SERVICE "jake-training-box:8000"
+#define LOG_SERVICE "http://jake-training-box.jakepoz.gmail.com.beta.tailscale.net:8000"
 
 #define ENCODER_BITRATE 8'000'000
 #define ENCODER_DEV "/dev/nvhost-msenc"
--- src/realtime/loguploader.py
+++ src/realtime/loguploader.py
@@ -2,18 +2,21 @@ import re
 import os
 import time
 import requests
+import logging
 
 from typing import Dict, Any
 from inotify_simple import INotify, flags
+from src.logutil import LogHashes
 
 # Watches for newly completed log files in the logging directory, and uploads them
 
-from src.logutil import LogHashes
+logger = logging.getLogger(__name__)
+
 
 def load_realtime_config() -> Dict[str, Any]:
     config_vars = {}
     with open(os.path.join(os.path.dirname(__file__), "..", "include", "config.h")) as f:
-        b = re.findall(r"\#define +([a-zA-Z0-9_]+) +\"?([0-9a-zA-Z_/\-\'\:]+)\"?", f.read())
+        b = re.findall(r"\#define +([a-zA-Z0-9_]+) +\"?([0-9a-zA-Z_/\-\'\:\.]+)\"?", f.read())
 
         for match in b:
             config_vars[match[0]] = match[1]
@@ -24,6 +27,10 @@ def load_realtime_config() -> Dict[str, Any]:
 CONFIG = load_realtime_config()
 
 def main():
+    req = requests.get(CONFIG["LOG_SERVICE"] + "/logs")
+    if req.status_code != 200:
+        logger.warn("Warning, unable to connect to logservice")
+
     lh = LogHashes(CONFIG["LOG_PATH"])
 
     inotify = INotify()
