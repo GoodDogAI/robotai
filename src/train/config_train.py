8367cb14330d7a7bef4996751abfb040d9b0608c
Jake <jakepoz@gmail.com>
2022-08-17 18:25:47 +0000

    Moving files to other drive, bag2log is way faster now for resizing images

--- src/train/config_train.py
+++ src/train/config_train.py
@@ -1,3 +1,3 @@
 import os
 
-RECORD_DIR = os.environ.get("ROBOTAI_RECORD_DIR", "/home/jake/robotairecords")
+RECORD_DIR = os.environ.get("ROBOTAI_RECORD_DIR", "/media/storage/robotairecords")
--- src/utils/bag2log.py
+++ src/utils/bag2log.py
@@ -1,10 +1,12 @@
 import os
+import glob
 import rosbag
 import numpy as np
 from cereal import log
 from src.video import create_video
 from src.include.config import load_realtime_config
-from skimage.transform import rescale
+import PIL
+from PIL import Image
 
 camera_topic = ["/camera/rgb/image_rect_raw"]
 CONFIG = load_realtime_config()
@@ -13,6 +15,7 @@ DECODE_WIDTH = int(CONFIG["CAMERA_WIDTH"])
 DECODE_HEIGHT = int(CONFIG["CAMERA_HEIGHT"])
 
 def convert(bag_file: str, logfile: str):
+    print(f"Converting {bag_file} to {logfile}")
     all_frames = []
     video_width, video_height = None, None
 
@@ -38,16 +41,18 @@ def convert(bag_file: str, logfile: str):
 
         for frame in all_frames:
             frame = frame.reshape((video_height, video_width, -1))
-            resized = rescale(frame, scale, channel_axis=2)
-            if resized.shape[0] != DECODE_HEIGHT:
-                l = (resized.shape[0] - DECODE_HEIGHT) // 2
-                resized = resized[l:resized.shape[0] - l,]
-            if resized.shape[1] != DECODE_WIDTH:
-                l = (resized.shape[1] - DECODE_WIDTH) // 2
-                resized = resized[:, l:resized.shape[1] - l]
+            frame = Image.fromarray(frame)
+            resized = frame.resize((round(video_width * scale), round(video_height*  scale)), 
+                                    resample=PIL.Image.Resampling.BILINEAR)
+            if resized.height != DECODE_HEIGHT:
+                l = (resized.height - DECODE_HEIGHT) // 2
+                resized = resized.crop((0, l, resized.width, resized.height - l))
+            if resized.width != DECODE_WIDTH:
+                l = (resized.width - DECODE_WIDTH) // 2
+                resized = resized.crop((l, 0, resized.width - l, resized.height))
 
+            resized = np.array(resized)
             resized = resized.reshape((resized.shape[0], -1))
-            resized = np.round(resized * 255).astype(np.uint8)
             resized_frames.append(resized)
 
         video_packets = create_video(resized_frames)
@@ -58,6 +63,16 @@ def convert(bag_file: str, logfile: str):
             dat.data = packet
             evt.write(lf)
 
+def _do_convert(path: str):
+    convert(path, path.replace("/media/storage/bagfiles/newbot_nov21/", "/media/storage/robotairecords/converted/").replace(".bag", ".log"))
+
 if __name__ == "__main__":
-    convert("/home/jake/bagfiles/newbot_nov21/record-brain-sac-atomic-cloud-515-20000-samp0.0_2022-05-08-16-29-52_0.bag", "/home/jake/robotairecords/record-brain-sac-atomic-cloud-515-20000-samp0.0_2022-05-08-16-29-52_0.log")
+    from multiprocessing import Pool
+
+    paths = glob.glob("/media/storage/bagfiles/newbot_nov21/*.bag")
+
+    #_do_convert(paths[0])
+    with Pool(processes=8) as pool:
+        pool.map(_do_convert, paths)
+       
 
