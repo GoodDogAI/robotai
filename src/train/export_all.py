797a8c0819320d0bf1ee8d75e5cbec7afbaf1710
Jake <jakepoz@gmail.com>
2022-08-25 20:31:56 +0000

    Basic export_all function, now needs to do some tests

---
+++ src/train/export_all.py
@@ -0,0 +1,49 @@
+import sys
+# Hack to allow loading the pickled yolov7 model
+sys.path.insert(0, "src/train/yolov7")
+
+import torch
+import onnx
+
+from src.train.yolov7.models.experimental import attempt_load
+from src.train.yolov7.utils.activations import Hardswish, SiLU
+from src.train.yolov7.utils.general import set_logging, check_img_size
+
+yolo_checkpoint = "/home/jake/robotai/_checkpoints/yolov7-tiny.pt"
+batch_size = 1
+img_size = (720, 1280)
+device = "cuda:0"
+
+
+
+# Load the original pytorch model
+model = attempt_load(yolo_checkpoint)
+labels = model.names
+
+# Checks
+gs = int(max(model.stride))  # grid size (max stride)
+img_size = [check_img_size(x, gs) for x in img_size]  # verify img_size are gs-multiples
+img = torch.zeros(batch_size, 3, *img_size).to(device)  # image size(1,3,320,192) iDetection
+model.model[-1].export = False  # set Detect() layer grid export
+y = model(img)  # dry run
+
+
+# Convert that to ONNX
+onnx_path = yolo_checkpoint.replace(".pt", ".onnx")
+print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
+model.eval()
+output_names = ['classes', 'boxes'] if y is None else ['output']
+
+
+torch.onnx.export(model, img, onnx_path, verbose=False, opset_version=12, input_names=['images'],
+                  output_names=output_names,
+                  dynamic_axes=None)
+
+# Checks
+onnx_model = onnx.load(onnx_path)  # load onnx model
+onnx.checker.check_model(onnx_model)  # check onnx model
+print("Confirmed ONNX model is valid")
+    
+# Convert the ONNX to TensorRT
+
+# Run both on the same random input and make sure
