6501af587201a796bea3fff06abc904ad972ff80
Jake <jakepoz@gmail.com>
2022-08-25 21:04:12 +0000

    Export all builds a trt engine on x64 now

--- src/train/export_all.py
+++ src/train/export_all.py
@@ -16,7 +16,6 @@ img_size = (720, 1280)
 device = "cuda:0"
 
 
-
 # Load the original pytorch model
 model = attempt_load(yolo_checkpoint)
 labels = model.names
@@ -46,8 +45,35 @@ onnx.checker.check_model(onnx_model)  # check onnx model
 print("Confirmed ONNX model is valid")
     
 # Convert the ONNX to TensorRT
+engine_path = yolo_checkpoint.replace(".pt", ".engine")
+
 print('\nStarting TensorRT export with onnx %s...' % onnx.__version__)
-
-
+TRT_LOGGER = trt.Logger()
+with trt.Builder(TRT_LOGGER) as builder, \
+     builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)) as network, \
+     builder.create_builder_config() as config, \
+     trt.OnnxParser(network, TRT_LOGGER) as parser, \
+     trt.Runtime(TRT_LOGGER) as runtime:
+     
+        config.max_workspace_size = 1 << 28  # 256MiB
+        builder.max_batch_size = 1
+        
+        with open(onnx_path, "rb") as model:
+            print("Beginning ONNX file parsing")
+            if not parser.parse(model.read()):
+                print("ERROR: Failed to parse the ONNX file.")
+                for error in range(parser.num_errors):
+                    print(parser.get_error(error))
+    
+        # The actual yolov3.onnx is generated with batch size 64. Reshape input to batch size 1
+        #network.get_input(0).shape = [1, 3, 608, 608]
+        print("Completed parsing of ONNX file")
+        print("Beginning engine build")
+        plan = builder.build_serialized_network(network, config)
+        engine = runtime.deserialize_cuda_engine(plan)
+        print("Completed creating Engine")
+        with open(engine_path, "wb") as f:
+            f.write(plan)
+            
 
 # Run both on the same random input and make sure
