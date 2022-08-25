5452e48c23f136372e7dd372bf39e33941cf0bfd
Jake <jakepoz@gmail.com>
2022-08-25 19:30:08 +0000

    More setup for yolo intermediate network

--- src/train/yolov7/models/yolo.py
+++ src/train/yolov7/models/yolo.py
@@ -8,13 +8,13 @@ from copy import deepcopy
 sys.path.append('./')  # to run '$ python *.py' files in subdirectories
 logger = logging.getLogger(__name__)
 import torch
-from .common import *
-from .experimental import *
-from .utils.autoanchor import check_anchor_order
-from .utils.general import make_divisible, check_file, set_logging
-from .utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
+from models.common import *
+from models.experimental import *
+from utils.autoanchor import check_anchor_order
+from utils.general import make_divisible, check_file, set_logging
+from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
-from .utils.loss import SigmoidBin
+from utils.loss import SigmoidBin
 
 try:
     import thop  # for FLOPS computation
