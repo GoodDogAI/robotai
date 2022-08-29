18b49bfe7de5b89470d940357bd4730a2be50f4e
Jake <jakepoz@gmail.com>
2022-08-29 18:34:33 +0000

    Fixing up dataloader test

--- src/tests/test_videoloader.py
+++ src/tests/test_videoloader.py
@@ -6,11 +6,13 @@ from torch.utils.data import DataLoader
 from src.train.videoloader import surface_to_tensor, build_datapipe
 
 class VideoLoaderTest(unittest.TestCase):
+    max_elements = 1000
+
     def test_basic(self):
         start = time.perf_counter()
         count = 0
 
-        for x in build_datapipe():
+        for x in build_datapipe().header(self.max_elements):
             count += 1
 
         print(f"Took {time.perf_counter() - start:0.2f} seconds")
@@ -18,7 +20,7 @@ class VideoLoaderTest(unittest.TestCase):
         print(f"{count / (time.perf_counter() - start):0.1f} fps")
   
     def test_train(self):
-        datapipe = build_datapipe()
+        datapipe = build_datapipe().header(self.max_elements)
         dl = DataLoader(dataset=datapipe)
 
 
