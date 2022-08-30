bdf4632e04a8c489a01578211b66d099bb7a900e
Jake <jakepoz@gmail.com>
2022-08-30 20:26:32 +0000

    Failing faster if config entry not found

--- src/config/dotdict.py
+++ src/config/dotdict.py
@@ -1,7 +1,19 @@
-from typing import Dict
+from typing import Any
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
     __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
+    __setattr__ = None
     __delattr__ = dict.__delitem__
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        raise NotImplementedError()
+
+    def __delattr__(self, __name: str) -> None:
+        raise NotImplementedError()
+    
+    def __getattr__(self, __name: str) -> Any:
+        if __name in self:
+            return self[__name]
+        else:
+            raise KeyError("Config entry '{}' not found".format(__name))
