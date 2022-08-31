5bc742fa1de665aaaf9ba4c78b98e82b88c4793d
Jake <jakepoz@gmail.com>
2022-08-31 19:21:51 +0000

    Generating reference inputs

---
+++ src/tests/test_modelservice.py
@@ -0,0 +1,47 @@
+import unittest
+import tempfile
+import hashlib
+import os
+
+from fastapi.testclient import TestClient
+from cereal import log
+from src.logutil import sha256, LogHashes
+from src.tests.utils import artificial_logfile
+from src.web.main import app
+from src.web.dependencies import get_loghashes
+
+
+class ModelServiceTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = TestClient(app)
+
+    def test_model_onnx(self):
+        resp = self.client.get("/models/brain/default/")
+        self.assertEqual(resp.status_code, 200)
+
+        j = resp.json()
+        main_key = list(j.keys())[0]
+
+        resp = self.client.get(f"/models/INVALID/onnx/")
+        self.assertEqual(resp.status_code, 404)
+
+        resp = self.client.get(f"/models/{j[main_key]['vision_model']}/onnx/")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_model_references(self):
+        resp = self.client.get("/models/brain/default/")
+
+        j = resp.json()
+        main_key = list(j.keys())[0]
+        
+        resp = self.client.get(f"/models/{j[main_key]['vision_model']}/reference_input/INVALID_NAME")
+        self.assertEqual(resp.status_code, 404)
+    
+        resp = self.client.get(f"/models/{j[main_key]['vision_model']}/reference_input/input.1")
+        self.assertEqual(resp.status_code, 200)
+
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
--- src/web/modelservice.py
+++ src/web/modelservice.py
@@ -1,8 +1,12 @@
 import os
+import onnxruntime
+import numpy as np
+import io
+from numpy.random import default_rng
 
 from typing import List
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse, FileResponse, Response
 
 from src.train.modelloader import create_and_validate_onnx
 
@@ -12,6 +16,9 @@ from src.config import HOST_CONFIG, MODEL_CONFIGS, BRAIN_CONFIGS
 router = APIRouter(prefix="/models",
     tags=["models"],
 )
+
+REFERENCE_MODEL_RNG_SEED = 17
+
 
 @router.get("/")
 async def get_all_models() -> JSONResponse:
@@ -36,12 +43,32 @@ async def get_model_onnx(model_name: str) -> FileResponse:
         raise HTTPException(status_code=404, detail="Model not found")
 
     onnx_path = create_and_validate_onnx(model_name)
-    return FileResponse(path=onnx_path, media_type='application/octet-stream', filename=os.path.basename(onnx_path))
+    return FileResponse(path=onnx_path, media_type="application/octet-stream", filename=os.path.basename(onnx_path))
 
 @router.get("/{model_name}/reference_input/{input_name}")
-async def get_model_reference_input(model_name: str) -> FileResponse:
-    pass
+async def get_model_reference_input(model_name: str, input_name: str) -> FileResponse:
+    if model_name not in MODEL_CONFIGS:
+        raise HTTPException(status_code=404, detail="Model not found")
 
+    onnx_path = create_and_validate_onnx(model_name)
+    ort_sess = onnxruntime.InferenceSession(onnx_path)
+
+    all_inputs = {x.name:x.shape for x in ort_sess.get_inputs()}
+
+    if input_name not in all_inputs:
+        raise HTTPException(status_code=404, detail="Input name not found in model ONNX")
+
+    shape = all_inputs[input_name]
+    rng = default_rng(REFERENCE_MODEL_RNG_SEED)
+    data = rng.random(shape)
+
+    file_data = io.BytesIO()
+    np.save(file_data, data)
+
+    return Response(content=file_data.getvalue(), media_type="application/octet-stream")
+    
+    
 @router.get("/{model_name}/reference_output/{output_name}")
-async def get_model_reference_input(model_name: str) -> FileResponse:
-    pass
+async def get_model_reference_input(model_name: str, output_name: str) -> FileResponse:
+    if model_name not in MODEL_CONFIGS:
+        raise HTTPException(status_code=404, detail="Model not found")
