7c6af036c7b85ee6542022b11bb18ef0ec96e20e
Jake <jakepoz@gmail.com>
2022-09-09 20:46:57 +0000

    Yay, color conversion onnx graph stuff is working

--- src/tests/test_onnx_yuv.py
+++ src/tests/test_onnx_yuv.py
@@ -15,11 +15,8 @@ class ONNXYUVTest(unittest.TestCase):
         # Start with random RGB image
         rgb_input = torch.randint(low=0, high=255, size=(720, 1280, 3), dtype=torch.uint8)
 
-        # Convert to YCbCr and back using a known reference implementation
+        # Convert to YCbCr using a known reference implementation
         yuv_input = torch.tensor(rgb2ycbcr(rgb_input.numpy()))
-        back_rgb = (torch.tensor(ycbcr2rgb(yuv_input.numpy())) * 255).float()
-
-        assert torch.equal(rgb_input, torch.round(back_rgb).to(torch.uint8))
 
         # Take the YUV image and split it into NV12M format
         y = yuv_input[:, :, 0]
@@ -32,20 +29,19 @@ class ONNXYUVTest(unittest.TestCase):
         rgb = nv12m_to_rgb(y, uv)
         onnx = get_onnx(nv12m_to_rgb, (y, uv))
 
-        # Compare the results
+        # Compare the results, for this, we actually have to take the NV12M UV plane, and reupsample it
+        u = uv[:, :, :, 0::2].repeat_interleave(2, dim=3).repeat_interleave(2, dim=2)
+        v = uv[:, :, :, 1::2].repeat_interleave(2, dim=3).repeat_interleave(2, dim=2)
+        yuv = torch.cat([y, u, v], dim=1).float() 
+        yuv = rearrange(yuv, "b c h w -> b h w c")[0]
+        back_rgb = (torch.tensor(ycbcr2rgb(yuv.numpy())) * 255).float()
+
         rgb = rearrange(rgb, "b c h w -> b h w c")[0] * 255
 
-        
+        diffs = torch.abs(rgb - back_rgb)
         assert torch.allclose(back_rgb, rgb, atol=1)
         print("Done")
-        # u = uv[:, :, :, 0::2].repeat_interleave(2, dim=3).repeat_interleave(2, dim=2)
-        # v = uv[:, :, :, 0::2].repeat_interleave(2, dim=3).repeat_interleave(2, dim=2)
-        # yuv = torch.cat([y, u, v], dim=1).float()
-        # yuv = np.transpose(yuv, (0, 2, 3, 1)).squeeze(0)
 
-        # reference = ycbcr2rgb(yuv.numpy())
-
-        # print(reference)
 
 
 
