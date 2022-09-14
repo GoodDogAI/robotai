1109e582a33db7e72b77cdbbede03ecce8acba56
Jake <jakepoz@gmail.com>
2022-09-14 21:51:14 +0000

    Refactors

--- src/tests/test_modelvalidation.py
+++ src/tests/test_modelvalidation.py
@@ -6,11 +6,11 @@ import unittest
 
 from src.config import HOST_CONFIG, BRAIN_CONFIGS
 from src.train.modelloader import create_and_validate_onnx, create_and_validate_trt
-from src.logutil import validate_log
+from src.train.log_validation import full_validate_log
 
 class TestModelValidation(unittest.TestCase):
     def test_vision_intermediate_video(self):
         test_path = os.path.join(HOST_CONFIG.RECORD_DIR, "unittest", "alphalog-4f642094-2022-9-14-16_24.log")
         
         with open(test_path, "rb") as f:
-            validate_log(f)
+            full_validate_log(f)
