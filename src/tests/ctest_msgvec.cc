e75043fff2d2fd9e709ad52260bd50f48fa473f9
Jake <jakepoz@gmail.com>
2022-09-29 23:30:17 +0000

    Trying some C++ debugging

--- src/tests/ctest_msgvec.cc
+++ src/tests/ctest_msgvec.cc
@@ -4,6 +4,8 @@
 #include <thread>
 #include <chrono>
 
+#include "cereal/messaging/messaging.h"
+
 #include "msgvec.h"
 #include "config.h"
 
@@ -11,8 +13,36 @@
 TEST_CASE( "Proess a simple message", "[msgvec]" ) {
    MsgVec msgvec{R"(
         {
-            "obs": [],
+            "obs": [{
+                "type": "msg",
+                "path": "voltage.volts",
+                "index": -1,
+                "timeout": 0.01,
+                "filter": {
+                    "field": "type",
+                    "op": "eq",
+                    "value": "mainBattery"
+                },
+                "transform": {
+                    "type": "rescale",
+                    "msg_range": [0, 13.5],
+                    "vec_range": [-1, 1]
+                }
+            }],
             "act": []
         }
     )"};
+
+    REQUIRE(msgvec.obs_size() == 1);
+
+    MessageBuilder vmsg;
+    auto vevent = vmsg.initEvent(true);
+    auto vdat = vevent.initVoltage();
+    vdat.setVolts(13.2);
+    vdat.setType(cereal::Voltage::Type::MAIN_BATTERY);
+
+    auto vwords = capnp::messageToFlatArray(vmsg);
+    auto msgr = capnp::readDataStruct<cereal::Event>(vwords);
+
+    msgvec.input(msgr);
 }
