60bd81fd43cf04fb5aa9866889535abea935d3d6
Jake Poznanski <jakepoz@gmail.com>
2022-10-11 19:35:23 -0700

    Manager fixes

--- src/realtime/manager.py
+++ src/realtime/manager.py
@@ -108,7 +108,7 @@ def get_procs(models: Dict[str,str]) -> List[ManagerProcess]:
         NativeProcess("simplebgcd"),
         NativeProcess("appcontrold"),
         NativeProcess("braind", ["--config", os.path.join(DEVICE_CONFIG.MODEL_STORAGE_PATH, "brain_config.json"),
-                                 "--vision_model", models["vision_model"]]),
+                                 "--vision_model", models["vision"]]),
     ]
 
 
