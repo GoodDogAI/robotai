b4669001f9029bfb12ea3ced0de7ad4278d9a7a9
Jake Poznanski <jakepoz@gmail.com>
2022-10-19 19:04:17 -0700

    Some prints

--- src/msgvec/msgvec.cc
+++ src/msgvec/msgvec.cc
@@ -415,6 +415,7 @@ MsgVec::TimeoutResult MsgVec::get_obs_vector(float *obsVector) {
             
                 if (cur_time - m_obsHistoryTimestamps[index][history_index] > (history_index + 1) * obs["timeout"].get<float>() * 1e9) {
                     if (i == 0) {
+                        //std::cerr << "Failure syncing " << obs["path"]  << " Cur time: " << cur_time << ", history time: " << m_obsHistoryTimestamps[index][history_index] << ", timeout: " << obs["timeout"].get<float>() << std::endl;
                         timestamps_valid = TimeoutResult::MESSAGES_NOT_READY;
                     } else if (timestamps_valid == TimeoutResult::MESSAGES_ALL_READY) {
                         timestamps_valid = TimeoutResult::MESSAGES_PARTIALLY_READY;
