634bb20d00ab2db07769b5639aa6b84a848b71b5
Jake <jakepoz@gmail.com>
2022-10-20 02:29:03 +0000

    Arrow model cache test

--- src/tests/test_arrowmodelcache.py
+++ src/tests/test_arrowmodelcache.py
@@ -1,5 +1,7 @@
 import os
+import numpy as np
 import unittest
+import itertools
 
 from src.train.modelloader import model_fullname
 from src.train.arrowcache import ArrowModelCache, ArrowRLDataset
@@ -16,10 +18,14 @@ class TestArrowRLCache(unittest.TestCase):
     def test_basic(self):
         cache = ArrowRLDataset(os.path.join(HOST_CONFIG.RECORD_DIR, "unittest"), MODEL_CONFIGS["basic-brain-test1"])
         
-        for entry in cache.generate_samples():
+        for entry in itertools.islice(cache.generate_samples(), 100):
             print(entry)
+
+        samples = list(itertools.islice(cache.generate_samples(), 100))
+
+        for index, sample in enumerate(samples[:-1]):
+            np.testing.assert_array_almost_equal(sample["next_obs"], samples[index + 1]["obs"])
 
     # Test that you wait for the obs vectors to populate, though this is somewhat helped by waiting for the first inference
     # TODO test the case of done flags being set on last entry, plus following the "on_reward_override" mode
     # TODO test reward overrides
-    # TODO test next_obs is correct
