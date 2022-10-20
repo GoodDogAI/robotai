5cf376191e3a01ac8280d49acba16e8f678ce349
Jake Poznanski <jakepoz@gmail.com>
2022-10-19 19:20:02 -0700

    Small brain code cleanup

--- src/include/util.h
+++ src/include/util.h
@@ -50,3 +50,13 @@ private:
   }
   inline static std::atomic<bool> do_exit = false;
 };
+
+inline bool endsWith(std::string_view str, std::string_view suffix)
+{
+    return str.size() >= suffix.size() && 0 == str.compare(str.size()-suffix.size(), suffix.size(), suffix);
+}
+
+inline bool startsWith(std::string_view str, std::string_view prefix)
+{
+    return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
+}
