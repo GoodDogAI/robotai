36765fadc96262b6b4c3ff991e007220da814d94
Jake <jakepoz@gmail.com>
2022-10-24 17:01:05 +0000

    Start tensorboard  more reliably

--- run_web.sh
+++ run_web.sh
@@ -11,18 +11,20 @@ fi
 # Start the backend server
 uvicorn src.web.main:app --host 0.0.0.0 --timeout-keep-alive 60 --reload &
 
-# Start the frontend server
-cd frontend
-npm start &
 
 # Start tensorboard
 tensorboard --logdir=./_sb3_logs --bind_all &
 
 # Save the PID of tensorboard
-tpid=$!
-
 # Send SIGTERM to tensorboard when this script exits
+tpid=$!
 trap "kill $tpid" EXIT
+
+
+# Start the frontend server
+cd frontend
+npm start &
+
 
 wait
 
