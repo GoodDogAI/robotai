1cf79d19418a3d520d470c1926d52bbf52bde0b4
Jake <jakepoz@gmail.com>
2022-11-07 17:36:24 +0000

    Improving logging of tensorboard stuff

--- src/train/stable_baselines_train.py
+++ src/train/stable_baselines_train.py
@@ -10,16 +10,15 @@ from gym import spaces
 from tqdm import tqdm
 
 from stable_baselines3 import SAC
-from stable_baselines3.common.logger import configure
-from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.logger import configure, HParam
 
-from wandb.integration.sb3 import WandbCallback
 
 from src.config import HOST_CONFIG, MODEL_CONFIGS
 from src.msgvec.pymsgvec import PyMsgVec, PyTimeoutResult, PyMessageTimingMode
 from src.train.rldataset import MsgVecDataset
 from stable_baselines3.common.buffers import ReplayBuffer
 from src.train.stable_baselines_buffers import HostReplayBuffer
+
 
 class MsgVecEnv(gym.Env):
     def __init__(self, msgvec) -> None:
@@ -33,10 +32,11 @@ if __name__ == "__main__":
     msgvec = PyMsgVec(brain_config["msgvec"], PyMessageTimingMode.REPLAY)
     cache = MsgVecDataset(os.path.join(HOST_CONFIG.RECORD_DIR), brain_config)
     log_dir = "/home/jake/robotai/_sb3_logs/"
-    validation_log = "alphalog-4425c446"  
+    buffer_size = 5_000
+    batch_size = 512
+    validation_runname = "alphalog-4425c446"  
     validation_buffer_size = 10_000
 
-    buffer_size = 50_000
 
     env = MsgVecEnv(msgvec)
     model = SAC("MlpPolicy", env, buffer_size=buffer_size, verbose=1, 
@@ -49,16 +49,31 @@ if __name__ == "__main__":
 
     # If run_name is not set, just create the next highest run1, run2, etc.. in the folder
     if run_name is None:
-        rundirs = glob.glob(os.path.join(log_dir, "run*"))
-        max_run = max([int(os.path.basename(d).replace("run", "")) for d in rundirs])
-        run_name = f"run{max_run + 1}"
+        try:
+            rundirs = glob.glob(os.path.join(log_dir, "run*"))
+            max_run = max([int(os.path.basename(d).replace("run", "")) for d in rundirs])
+            run_name = f"run{max_run + 1}"
+        except ValueError:
+            run_name = "run1"
 
     # Setup the logger
-    logger = configure(os.path.join(log_dir, run_name), ["stdout", "tensorboard"])
+    logger = configure(os.path.join(log_dir, run_name), ["tensorboard"])
     model.set_logger(logger)
 
-    # TODO Log the hyperparameters
-    #https://stable-baselines3.readthedocs.io/en/master/guide/tensorboard.html
+    # Log the hyperparameters
+    # https://stable-baselines3.readthedocs.io/en/master/guide/tensorboard.html
+    logger.record("hparams", HParam(hparam_dict={
+        "algorithm": model.__class__.__name__,
+        "buffer_size": buffer_size,
+        "batch_size": batch_size,
+        "learning_rate": model.learning_rate,
+        "target_entropy": model.target_entropy,
+        "validation_runname": validation_runname,
+        "validation_buffer_size": validation_buffer_size,
+    }, metric_dict={
+        "train/actor_loss": 0,
+        "train/critic_loss": 0,
+    }))
 
     # Copy the current file to the log directory, as a reference
     with open(__file__, "r") as f:
@@ -76,16 +91,46 @@ if __name__ == "__main__":
     print(f"Added {samples_added} samples to the replay buffer")
 
     # Fill the validation replay buffer
-    validation_buffer = ReplayBuffer(buffer_size=validation_buffer_size, handle_timeout_termination=False)
-    
+    validation_buffer = ReplayBuffer(buffer_size=validation_buffer_size,
+                                     observation_space=env.observation_space, action_space=env.action_space, handle_timeout_termination=False)
+    groups = cache.lh.group_logs()
+    valgroup = next(g for g in groups if g[0].get_runname() == validation_runname)
 
-    for i in range(1000*1000):
-        model.train(gradient_steps=1000*10, batch_size=512) #*10 added in run23
-        print("Trained 1000 steps")
+    for entry in cache.generate_log_group(valgroup, shuffle_within_group=False):
+        validation_buffer.add(obs=entry["obs"], action=entry["act"], reward=entry["reward"], next_obs=entry["next_obs"], done=entry["done"], infos=None)
 
-        # 
+    observation_stds = torch.from_numpy(validation_buffer.observations[:validation_buffer.size(), 0].std(axis=0)).to(model.device)
 
-        logger.dump(step=i)
+    print(f"Added {validation_buffer.size()} samples to the validation buffer from {validation_runname}")
+
+    for step in range(1000*1000):
+        num_updates = round(buffer_size * 5 / batch_size)
+        model.train(gradient_steps=num_updates, batch_size=batch_size)
+        print(f"Trained {num_updates} steps")
+
+        # Run the actor against the entire validation buffer, and measure the variance of the actions
+        validation_acts = []
+        perturbed_acts = []
+        for i in range(0, validation_buffer.size(), batch_size):
+            obs = torch.from_numpy(validation_buffer.observations[i:i+batch_size, 0]).to(model.device)
+            validation_acts.append(model.actor(obs, deterministic=True).detach().cpu())
+
+            # Perturb the observations and see how much the output changes
+            perturbed_obs = obs + torch.randn_like(obs, device=obs.device) * observation_stds * 0.1
+            perturbed_acts.append(model.actor(perturbed_obs, deterministic=True).detach().cpu())
+            logger.record_mean("validation/perturbed_act_diff", torch.mean(torch.abs(validation_acts[-1] - perturbed_acts[-1])))
+
+        # Calculate and report statistics against the validation buffer
+        validation_acts = torch.concat(validation_acts)
+        validation_act_mean = torch.mean(validation_acts, axis=0)
+        validation_act_var = torch.var(validation_acts, axis=0)
+        for i, act in enumerate(brain_config["msgvec"]["act"]):
+            logger.record(f"validation/{act['path'].replace('.', '_')}_mean", validation_act_mean[i].item())
+            logger.record(f"validation/{act['path'].replace('.', '_')}_var", validation_act_var[i].item())
+            logger.record(f"validation/{act['path'].replace('.', '_')}_stddev", torch.sqrt(validation_act_var[i]).item())
+            logger.record(f"validation/{act['path'].replace('.', '_')}_hist", validation_acts[:, i])            
+
+        logger.dump(step=step)
 
         # Each step, replace 50% of the replay buffer with new samples
         for entry in tqdm(itertools.islice(cache.generate_samples(), buffer_size // 2), desc="Refill buffer", total=buffer_size // 2):
